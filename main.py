9ec330d6f23f38dadb7c9283f38959119f773b10
KColagi1 <kcolagi1@its.jnj.com>
2024-04-25 17:41:13 -0700

    Adding all the subplots and starting to resize it.

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # Video Source: https://www.youtube.com/watch?v=7eh4d6sabA0&t=345s
-
+# Linear Regression video: https://www.youtube.com/watch?v=lGg0LNZplVQ
 
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error, accuracy_score
+from sklearn.linear_model import LinearRegression
 import joblib
 from sklearn import tree
 
@@ -22,18 +23,31 @@ y = df['Outcome']  # Output data
 neg = X[df['Outcome'] == 0]
 pos = X[df['Outcome'] == 1]
 
+print(f'Min Values: {X.min().to_numpy()}')
+print(f'Avg Neg Values: {neg.mean().to_numpy()}')
+print(f'Avg Pos Values: {pos.mean().to_numpy()}')
+print(f'Max Values: {X.max().to_numpy()}')
+
 # Split the data set into two random sets, one for training, one for testing.
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 # Leaning and Predicting (Using Decision Tree)
-model = DecisionTreeClassifier()  # Define the data model
-model.fit(X, y)  # Train the model with training data set
-# model.fit(X_train, y_train)  # Train the model with training data set
-# predictions = model.predict(X_test)  # Ask for a prediction with test data set
-# predictions = model.predict([should_be_diabetic, should_not_be_diabetic])
 
-# Calculating the Accuracy
-# ac_score = accuracy_score(y_test, predictions)
+# Define the data model
+model = DecisionTreeClassifier()
+# model = LinearRegression()
+# model.fit(X, y)  # Train the model with training data set
+model.fit(X_train, y_train)  # Train the model with training data set
+prediction = model.predict(X_test)  # Ask for a prediction with test data set
+# prediction = model.predict([should_be_diabetic, should_not_be_diabetic])
+
+# Calculating the Accuracy Score
+ac_score = accuracy_score(y_test, prediction)
+
+# Evaluate the performance of the model
+r2 = r2_score(y_test, prediction)
+# mean_sqr_err = mean_squared_error(y_test, prediction)
+# mean_abs_err = mean_absolute_error(y_test, prediction)
 
 # Persisting Models
 # joblib.dump(model, 'diabetes_predictions.joblib')  # Save a binary file with trained model data
@@ -63,90 +77,146 @@ print(f'\nDescribe DF:\n{X.describe()}')
 print(f'\nDescribe neg:\n{neg.describe()}')
 print(f'\nDescribe pos:\n{pos.describe()}')
 
-# print(f'\nDataframe Averages:\n {df.mean()}')
-# print(f'\nNegative Averages:\n {neg.mean()}')
-# print(f'\nPositive Averages:\n {pos.mean()}')
-
-
 # DF Values
 # print(f'\nDF Values:\n{df.values}')
-
 
 # Print Prepared Data
 # print(f'Input Data Frame:\n{X}')
 
-
 # Print the prediction(s)
-# print(f'Prediction(s): {predictions}')
+print(f'\nPrediction(s):\n{prediction}')
 
+# Model Performance
+print(f'\nr2 Score: {r2}')
+# print(f'Mean Squared Error: {mean_sqr_err}')
+# print(f'Mean Absolute Error: {mean_abs_err}')
+
+# Linear Regression
+# print(f'Intercept: {model.intercept_}')
+# print(f'Coefficient: {model.coef_}')
 
 # Print the Accuracy Score
-# print(f'Accuracy Score: {ac_score}')
+print(f'Accuracy Score: {ac_score}')
 
 
 # ---------- Plot Data ----------
 labels = ['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin',  'BMI', 'DiabetesPedigreeFunction', 'Age']
-# plt.title('Diabetic vs Non-Diabetic Mean Values')
-fig, ax = plt.subplots(8, 1)
+fig, ax = plt.subplots(8, 1, constrained_layout=True)#, height_ratios=[1.5, 1.5,1.5,1.5,1.5,1.5,1.5,1.5])
 
-# Pregnancies
-ax[0].barh(1, pos['Pregnancies'].mean())
-ax[0].barh(1, neg['Pregnancies'].mean())
-# plt.subplot(8, 1, 1)
-# plt.barh(1, pos['Pregnancies'].mean())
-# plt.barh(1, neg['Pregnancies'].mean())
-ax[0].set_title(labels[0])
+for data_point in range(len(labels)):
+    print(f'data_point is: {data_point}')
+    print(f'labels[data_point] is: {labels[data_point]}')
+    ax[data_point].barh(1, pos[labels[data_point]].mean())
+    ax[data_point].barh(1, neg[labels[data_point]].mean())
+    ax[data_point].set_title(labels[data_point])
+    # ax[data_point].yticks(ticks=[])
+
+# plt.xticks(ticks=[])
+# plt.yticks(ticks=[])
+# fig.tight_layout(pad=15.0)
+# plt.subplots_adjust(bottom=0.1, top=0.9, hspace=1.0)
+# plt.subplot_tool()
+
+fig = figsize=(2,2)
+plt.show()
+
+# # Pregnancies
+# ax[0].barh(1, pos['Pregnancies'].mean())
+# ax[0].barh(1, neg['Pregnancies'].mean())
+# ax[0].set_title(labels[0])
 # ax[0].yticks(ticks=[])
 
+# # plt.subplot(8, 1, 1)
+# # plt.barh(1, pos['Pregnancies'].mean())
+# # plt.barh(1, neg['Pregnancies'].mean())
+#
 # # Glucose
-# plt.subplot(8, 1, 2)
-# plt.barh(1, pos['Glucose'].mean())
-# plt.barh(1, neg['Glucose'].mean())
-# plt.ylabel(labels[1])
-# plt.yticks(ticks=[])
+# ax[0].barh(1, pos['Glucose'].mean())
+# ax[0].barh(1, neg['Glucose'].mean())
+# ax[0].set_title(labels[1])
+# ax[0].yticks(ticks=[])
+#
+# # plt.subplot(8, 1, 2)
+# # plt.barh(1, pos['Glucose'].mean())
+# # plt.barh(1, neg['Glucose'].mean())
+# # plt.ylabel(labels[1])
+# # plt.yticks(ticks=[])
 #
 # # Blood Pressure
-# plt.subplot(8, 1, 3)
-# plt.barh(1, pos['BloodPressure'].mean())
-# plt.barh(1, neg['BloodPressure'].mean())
-# plt.ylabel(labels[2])
-# plt.yticks(ticks=[])
+# ax[0].barh(1, pos['Pregnancies'].mean())
+# ax[0].barh(1, neg['Pregnancies'].mean())
+# ax[0].set_title(labels[0])
+# ax[0].yticks(ticks=[])
+#
+# # plt.subplot(8, 1, 3)
+# # plt.barh(1, pos['BloodPressure'].mean())
+# # plt.barh(1, neg['BloodPressure'].mean())
+# # plt.ylabel(labels[2])
+# # plt.yticks(ticks=[])
 #
 # # Skin Thickness
-# plt.subplot(8, 1, 4)
-# plt.barh(1, pos['SkinThickness'].mean())
-# plt.barh(1, neg['SkinThickness'].mean())
-# plt.ylabel(labels[3])
-# plt.yticks(ticks=[])
+# ax[0].barh(1, pos['Pregnancies'].mean())
+# ax[0].barh(1, neg['Pregnancies'].mean())
+# ax[0].set_title(labels[0])
+# ax[0].yticks(ticks=[])
+#
+# # plt.subplot(8, 1, 4)
+# # plt.barh(1, pos['SkinThickness'].mean())
+# # plt.barh(1, neg['SkinThickness'].mean())
+# # plt.ylabel(labels[3])
+# # plt.yticks(ticks=[])
 #
 # # Insulin
-# plt.subplot(8, 1, 5)
-# plt.barh(1, pos['Insulin'].mean())
-# plt.barh(1, neg['Insulin'].mean())
-# plt.ylabel(labels[4])
-# plt.yticks(ticks=[])
+# ax[0].barh(1, pos['Pregnancies'].mean())
+# ax[0].barh(1, neg['Pregnancies'].mean())
+# ax[0].set_title(labels[0])
+# ax[0].yticks(ticks=[])
+#
+# # plt.subplot(8, 1, 5)
+# # plt.barh(1, pos['Insulin'].mean())
+# # plt.barh(1, neg['Insulin'].mean())
+# # plt.ylabel(labels[4])
+# # plt.yticks(ticks=[])
 #
 # # BMI
-# plt.subplot(8, 1, 6)
-# plt.barh(1, pos['BMI'].mean())
-# plt.barh(1, neg['BMI'].mean())
-# plt.ylabel(labels[5])
-# plt.yticks(ticks=[])
+# ax[0].barh(1, pos['Pregnancies'].mean())
+# ax[0].barh(1, neg['Pregnancies'].mean())
+# ax[0].set_title(labels[0])
+# ax[0].yticks(ticks=[])
+#
+# # plt.subplot(8, 1, 6)
+# # plt.barh(1, pos['BMI'].mean())
+# # plt.barh(1, neg['BMI'].mean())
+# # plt.ylabel(labels[5])
+# # plt.yticks(ticks=[])
 #
 # # 'Diabetes Pedigree Function'
-# plt.subplot(8, 1, 7)
-# plt.barh(1, pos['DiabetesPedigreeFunction'].mean())
-# plt.barh(1, neg['DiabetesPedigreeFunction'].mean())
-# plt.ylabel(labels[6])
-# plt.yticks(ticks=[])
+# ax[0].barh(1, pos['Pregnancies'].mean())
+# ax[0].barh(1, neg['Pregnancies'].mean())
+# ax[0].set_title(labels[0])
+# ax[0].yticks(ticks=[])
+#
+# # plt.subplot(8, 1, 7)
+# # plt.barh(1, pos['DiabetesPedigreeFunction'].mean())
+# # plt.barh(1, neg['DiabetesPedigreeFunction'].mean())
+# # plt.ylabel(labels[6])
+# # plt.yticks(ticks=[])
 #
 # # Age
-# plt.subplot(8, 1, 8)
-# plt.barh(1, pos['Age'].mean())
-# plt.barh(1, neg['Age'].mean())
-# plt.ylabel(labels[7])
-# plt.yticks(ticks=[])
+# ax[0].barh(1, pos['Pregnancies'].mean())
+# ax[0].barh(1, neg['Pregnancies'].mean())
+# ax[0].set_title(labels[0])
+# ax[0].yticks(ticks=[])
+#
+# # plt.subplot(8, 1, 8)
+# # plt.barh(1, pos['Age'].mean())
+# # plt.barh(1, neg['Age'].mean())
+# # plt.ylabel(labels[7])
+# # plt.yticks(ticks=[])
 
-plt.xticks(ticks=[])
-plt.yticks(ticks=[])
-plt.show()
+# plt.xticks(ticks=[])
+# plt.yticks(ticks=[])
+# fig.tight_layout(pad=15.0)
+# plt.subplots_adjust(bottom=0.1, top=0.9, hspace=1.0)
+# plt.subplot_tool()
+# plt.show()
