f758feb0f85622fc39061923086efa1764b46244
KColagi1 <kcolagi1@its.jnj.com>
2024-05-10 17:30:37 -0700

    Made some changes to the histogram and scatter plot axis ticks. Changed the bar plot from showing all columns, to just one at a time by selecting the column from the drop down like the other plots and added some extra formatting to make it look nicer. Moved the column selection drop down in the plot window, which could use some fine tuning.

--- plot_data.py
+++ plot_data.py
@@ -48,7 +48,7 @@ class PlotData(QWidget):
         self.close_plot_window_button = QPushButton('Close Window')
 
         # Defining a label for the dropdown(ComboBox)
-        self.select_a_plot_label = QLabel('Select Data to Plot')
+        self.select_a_plot_label = QLabel('Select Data to Plot:')
 
         # Define progress bar
         self.progress_bar = QProgressBar(self)
@@ -77,20 +77,21 @@ class PlotData(QWidget):
 
         # Define group box
         plot_groupbox = QGroupBox()
-        plot_groupbox.setFixedWidth(730)
+        plot_groupbox.setFixedWidth(self.width - 20)
 
         # Adding widgets to the plot selection horizontal layout
-        plot_selection_layout.addWidget(self.bar_graph_button)
+        # plot_selection_layout.addWidget(self.bar_graph_button)
         plot_selection_layout.addWidget(plot_groupbox)
 
         # Adding and defining layouts
         scatter_plot_groupbox_layout = QGridLayout()
         plot_groupbox.setLayout(scatter_plot_groupbox_layout)
 
-        scatter_plot_groupbox_layout.addWidget(self.scatter_plot_button, 0, 0, 2, 1)
-        scatter_plot_groupbox_layout.addWidget(self.select_a_plot_label, 0, 1)
-        scatter_plot_groupbox_layout.addWidget(self.select_plot_dropdown, 1, 1)
-        scatter_plot_groupbox_layout.addWidget(self.new_plot_button, 0, 2, 2, 1)
+        scatter_plot_groupbox_layout.addWidget(self.bar_graph_button, 1, 0, 1, 2)
+        scatter_plot_groupbox_layout.addWidget(self.scatter_plot_button, 1, 2, 1, 2)
+        scatter_plot_groupbox_layout.addWidget(self.select_a_plot_label, 0, 0)
+        scatter_plot_groupbox_layout.addWidget(self.select_plot_dropdown, 0, 1)
+        scatter_plot_groupbox_layout.addWidget(self.new_plot_button, 1, 4, 1, 2)
 
         # Adding widgets to the plot window vertical layout
         plot_window_layout.addWidget(self.canvas)
@@ -116,7 +117,7 @@ class PlotData(QWidget):
         self.select_a_plot_label.setAlignment(Qt.AlignHCenter)
 
         # Setting button width
-        self.select_plot_dropdown.setFixedWidth(210)
+        # self.select_plot_dropdown.setFixedWidth(210)
         self.bar_graph_button.setFixedHeight(self.graph_button_height)
         self.scatter_plot_button.setFixedHeight(self.graph_button_height)
         self.new_plot_button.setFixedHeight(self.graph_button_height)
@@ -125,39 +126,112 @@ class PlotData(QWidget):
 
     def bar_graph(self):
 
+        # ======================================= Original Code ========================================================
+        # ptd = ProcessAndTrainData(self.data_frame)
+        # # ptd.X, y, neg, pos = ptd.prepare_data()
+        #
+        # # Clearing figure so current one can be displayed
+        # self.figure.clear()
+        #
+        # # Plot a bar graph that displays all the data.
+        # for data_point in range(len(self.labels)):
+        #
+        #     progress_status = int(((data_point + 1) / len(self.labels)) * 100)
+        #     self.progress_bar.setValue(progress_status)
+        #     self.progress_bar.setFormat(f'Processing Data: {progress_status + int(100 / len(self.labels)) + 1}%')
+        #     print(f'Bar Graph Loading Progress: {progress_status}%')
+        #
+        #     # Create an axis
+        #     ax = self.figure.add_subplot(8, 1, data_point + 1)
+        #
+        #     # Plot data
+        #     ax.barh(1, ptd.X[self.labels[data_point]].max(), color='red')
+        #     ax.barh(1, ptd.pos[self.labels[data_point]].mean(), color='blue')
+        #     ax.barh(1, ptd.neg[self.labels[data_point]].mean(), color='orange')
+        #     ax.barh(1, ptd.X[self.labels[data_point]].min(), color='green')
+        #     ax.set_title(self.labels[data_point], loc='left')
+        #     ax.tick_params(left=False, labelleft=False)  # Remove y-axis tick marks and labels
+        #
+        # # Display the legend
+        # self.figure.legend(['Max Values', 'Diabetes Avg', 'Non-Diabetes Avg', 'Min Values'])
+        #
+        # # Draw the graph on the canvas
+        # self.canvas.draw()
+        # self.progress_bar.reset()
+        #
+        # ==============================================================================================================
+
+        # ========================================== New Code ==========================================================
+
+        def addlabels(x, y):
+            for i in range(len(x)):
+                plt.text(i, y[i], y[i], ha='center')
+
         ptd = ProcessAndTrainData(self.data_frame)
-        # ptd.X, y, neg, pos = ptd.prepare_data()
 
         # Clearing figure so current one can be displayed
         self.figure.clear()
 
-        # Plot a bar graph that displays all the data.
-        for data_point in range(len(self.labels)):
+        # Create an axis
+        ax = self.figure.add_subplot(1, 1, 1)
 
-            progress_status = int(((data_point + 1) / len(self.labels)) * 100)
-            self.progress_bar.setValue(progress_status)
-            self.progress_bar.setFormat(f'Processing Data: {progress_status + int(100 / len(self.labels)) + 1}%')
-            print(f'Bar Graph Loading Progress: {progress_status}%')
+        column_index = self.select_plot_dropdown.currentIndex()
 
-            # Create an axis
-            ax = self.figure.add_subplot(8, 1, data_point + 1)
 
-            # Plot data
-            ax.barh(1, ptd.X[self.labels[data_point]].max(), color='red')
-            ax.barh(1, ptd.pos[self.labels[data_point]].mean(), color='blue')
-            ax.barh(1, ptd.neg[self.labels[data_point]].mean(), color='orange')
-            ax.barh(1, ptd.X[self.labels[data_point]].min(), color='green')
-            ax.set_title(self.labels[data_point], loc='left')
-            ax.tick_params(left=False, labelleft=False)  # Remove y-axis tick marks and labels
+        X_max = ptd.X[self.labels[column_index]].max()
+        X_min = ptd.X[self.labels[column_index]].min()
+        neg_avg = ptd.neg[self.labels[column_index]].mean()
+        pos_avg = ptd.pos[self.labels[column_index]].mean()
+
+        if self.labels[column_index] == 'BMI':
+            X_max = round(X_max, 1)
+            pos_avg = round(pos_avg, 1)
+            neg_avg = round(neg_avg, 1)
+            X_min = round(X_min, 1)
+        elif self.labels[column_index] == 'DiabetesPedigreeFunction':
+            X_max = round(X_max, 2)
+            pos_avg = round(pos_avg, 2)
+            neg_avg = round(neg_avg, 2)
+            X_min = round(X_min, 2)
+        else:
+            X_max = int(X_max)
+            pos_avg = int(pos_avg)
+            neg_avg = int(neg_avg)
+            X_min = int(X_min)
+
+        # Plot data
+        ax.bar(1, X_max, color='red', width=1)
+        ax.text(1, X_max / 2, str(X_max), ha='center')
+
+        ax.bar(2, pos_avg, color='orange', width=1)
+        ax.text(2, pos_avg / 2, str(pos_avg), ha='center')
+
+        ax.bar(3, neg_avg, color='yellow', width=1)
+        ax.text(3, neg_avg / 2, str(neg_avg), ha='center')
+
+        ax.bar(4, X_min, color='green', width=1)
+        if X_min < 0.1:
+            ax.text(4, X_min, str(X_min), ha='center')
+        else:
+            ax.text(4, X_min / 2, str(X_min), ha='center')
+
+        ax.set_title(self.labels[column_index], loc='left')
+        ax.tick_params(bottom=False, labelbottom=False)  # Remove x-axis tick marks and labels
 
         # Display the legend
         self.figure.legend(['Max Values', 'Diabetes Avg', 'Non-Diabetes Avg', 'Min Values'])
 
         # Draw the graph on the canvas
         self.canvas.draw()
-        self.progress_bar.reset()
+
+        # ==============================================================================================================
 
     def scatter_plot(self):
+
+        zero_count = 0
+        percent_dropped = 0
+        column_index = self.select_plot_dropdown.currentIndex()
+        min_data_frame_value = self.data_frame[self.labels[column_index]].max()
 
         # Clearing old figure
         self.figure.clear()
@@ -166,18 +240,13 @@ class PlotData(QWidget):
 
         ax.clear()
 
-        column_index = self.select_plot_dropdown.currentIndex()
-
         print(f'\n\nStarting to process the {self.labels[column_index]} column')
 
-        zero_count = 0
-
-        # PLot each data point in the column.
+        # Filter and plot each data point in the column.
         for data_point in range(len(self.data_frame.to_numpy())):
-            # print(column_index, data_point)
 
-            progress_status = int(((data_point + 1) / len(self.data_frame.to_numpy())) * 100)
-
+            # Progress bar
+            progress_status = int(((data_point + 1) / self.data_frame.shape[0]) * 100)
             if progress_status <= 100:
                 print(
                     f'Scatter Plot Loading Progress: {int(((data_point + 1) / len(self.data_frame.to_numpy())) * 100)}%'
@@ -192,17 +261,38 @@ class PlotData(QWidget):
                 color = 'red'
 
             # Remove the data points with a 0 value in all columns, EXCEPT pregnancies.
-            if self.data_frame.to_numpy()[data_point][column_index] > 0 and column_index > 0:
+            if self.data_frame.to_numpy()[data_point][column_index] > 0 and self.labels[column_index] != 'Pregnancies':
+
+                # Determining the min value for the data frame
+                if self.data_frame.to_numpy()[data_point][column_index] < min_data_frame_value:
+                    min_data_frame_value = self.data_frame.to_numpy()[data_point][column_index]
+                else:
+                    self.data_frame.to_numpy()[data_point][column_index] = min_data_frame_value
+
+                # Calculating the steps to the x-axis ticks
+                if self.labels[column_index] == 'BMI':
+                    steps = round((self.data_frame[self.labels[column_index]].max() - min_data_frame_value) / 10, 1)
+                elif self.labels[column_index] == 'DiabetesPedigreeFunction':
+                    steps = round((self.data_frame[self.labels[column_index]].max() - min_data_frame_value) / 10, 2)
+                else:
+                    steps = (self.data_frame[self.labels[column_index]].max() - min_data_frame_value) // 10
+
                 ax.scatter(self.data_frame.to_numpy()[data_point][column_index], data_point, color=color)
+                ax.xaxis.set_ticks(
+                    np.arange(min_data_frame_value, self.data_frame[self.labels[column_index]].max(), steps)
+                )
                 zero_count += 1
+                percent_dropped = round((self.data_frame.shape[0] - zero_count) / self.data_frame.shape[0] * 100, 1)
 
             # Plot all pregnancy data.
-            elif column_index == 0:
+            if self.labels[column_index] == 'Pregnancies':
                 ax.scatter(self.data_frame.to_numpy()[data_point][column_index], data_point, color=color)
-                zero_count = len(self.data_frame.to_numpy())
+                ax.xaxis.set_ticks(np.arange(0, 18, 1))
+                zero_count = self.data_frame.shape[0]
+                percent_dropped = 0
 
-        print(f'Showing the {self.labels[column_index]} plot. ({len(self.data_frame.to_numpy()) - zero_count}'
-              ' zero points have been dropped)')
+        print(f'Showing the {self.labels[column_index]} plot. ({self.data_frame.shape[0] - zero_count}'
+              f' zero points have been dropped({percent_dropped}%))')
         ax.legend(['Has diabetes', 'Does not have diabetes'], loc='upper right')
         ax.set_title(self.labels[column_index])
         ax.tick_params(left=False, labelleft=False)  # Remove y-axis tick marks and labels
@@ -211,11 +301,6 @@ class PlotData(QWidget):
         self.progress_bar.reset()
 
     def histogram_plot(self):
-
-        # self.data_frame['Pregnancies'].hist()
-        # plt.show()
-
-        # =======================================================================
 
         # Clearing old figure
         self.figure.clear()
@@ -228,49 +313,27 @@ class PlotData(QWidget):
 
         print(f'\n\nStarting to process the {self.labels[column_index]} column')
 
-        # =======================================================================
-
-        # zero_count = 0
-
-        # PLot each data point in the column.
-        # for data_point in range(len(self.data_frame.to_numpy())):
-
-        # progress_status = int(((data_point + 1) / len(self.data_frame.to_numpy())) * 100)
-
-        # if progress_status <= 100:
-        #     print(
-        #         f'Histogram Plot Loading Progress:'
-        #         f' {int(((data_point + 1) / len(self.data_frame.to_numpy())) * 100)}%'
-        #     )
-        #     self.progress_bar.setValue(progress_status)
-        #     self.progress_bar.setFormat(f'Processing Data: {progress_status + 1}%')
-
-        # Plot the data points in green when the patient doesn't have diabetes and red then they do.
-        # if self.data_frame.to_numpy()[data_point][8] == 0:  # Does not have diabetes.
-        #     color = 'green'
-        # else:  # Has diabetes.
-        #     color = 'red'
-
-        # Remove the data points with a 0 value in all columns, EXCEPT pregnancies.
-        # if self.data_frame.to_numpy()[data_point][column_index] > 0 and column_index > 0:
-        #     ax.hist(self.data_frame.to_numpy()[data_point][column_index])
-            # zero_count += 1
-
-        # Plot all pregnancy data.
-        # elif column_index == 0:
-        #     ax.hist(self.data_frame.to_numpy()[data_point][column_index])
-        #     zero_count = len(self.data_frame.to_numpy())
-
-        # print(f'Showing the {self.labels[column_index]} plot. ({len(self.data_frame.to_numpy()) - zero_count}'
-        #       ' zero points have been dropped)')
-
-        # ============================================================================
-
         ax.set_title(self.labels[column_index])
-        # ax.tick_params(left=False, labelleft=False)  # Remove y-axis tick marks and labels
 
-        ax.hist(self.data_frame[self.labels[column_index]].to_numpy(), bins=17)
+        # Drop all zero values in every column, except for the pregnancy column. Also define # of bins and x ticks.
+        if self.labels[column_index] == 'Pregnancies':
+            bins = 17
+            no_zeros_data_frame = self.data_frame[self.labels[column_index]]
+            ax.xaxis.set_ticks(np.arange(0, bins, 1))
+        else:
+            bins = 20
+            no_zeros_data_frame = self.data_frame[
+                self.labels[column_index]][self.data_frame[self.labels[column_index]] != 0]
+
+            if self.labels[column_index] == 'BMI':
+                steps = round(((no_zeros_data_frame.max() - no_zeros_data_frame.min()) / 10), 1)
+            elif self.labels[column_index] == 'DiabetesPedigreeFunction':
+                steps = round(((no_zeros_data_frame.max() - no_zeros_data_frame.min()) / 10), 2)
+            else:
+                steps = (no_zeros_data_frame.max() - no_zeros_data_frame.min()) // 10
+
+            ax.xaxis.set_ticks(np.arange(round(no_zeros_data_frame.min(), 2), no_zeros_data_frame.max(), steps))
+
+        ax.hist(no_zeros_data_frame.to_numpy(), bins=bins)
         self.canvas.draw()
         # self.progress_bar.reset()
-
-
